a72825c1ddd0fab86bd172678f217506be268ac3
Jun Ping Quek <quekjunping@gmail.com>
2020-11-13 09:05:19 +0800

    Add project files.

---
+++ FSD_Helpderly/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FSD_Helpderly.Models;
+using FSD_Helpderly.DAL;
+using Google.Type;
+using Google.Cloud.Firestore.V1;
+using Google.Cloud.Firestore;
+
+namespace FSD_Helpderly.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VolunteerViewPost()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FSD_Helpderly/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FSD_Helpderly.Models;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp.Response;
+
+namespace FSD_Helpderly.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Register/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add volunteer record to database
+
+                TempData["Message"] = "Your Account have been successfully created!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //Input validation fails, return to the register view to display error message
+                return View(register);
+            }
+        }
+    }
+}
